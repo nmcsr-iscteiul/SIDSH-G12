24046f4bb88a3f6e9df91664cd10e53b3766705f
fhcpc-iscteiul <55882936+fhcpc-iscteiul@users.noreply.github.com>
2020-05-31 10:21:50 +0100

    requirement almost done only redirect missing

---
+++ covid-sci-discoveries/src/main/java/covid_sci_discoveries/covid_sci_discoveries/App.java
@@ -0,0 +1,109 @@
+package covid_sci_discoveries.covid_sci_discoveries;
+
+import org.jdom.Element;
+import pl.edu.icm.cermine.ContentExtractor;
+import pl.edu.icm.cermine.content.model.ContentStructure;
+import pl.edu.icm.cermine.exception.AnalysisException;
+import pl.edu.icm.cermine.exception.TransformationException;
+import pl.edu.icm.cermine.metadata.affiliation.CRFAffiliationParser;
+import pl.edu.icm.cermine.metadata.model.DateType;
+import pl.edu.icm.cermine.metadata.model.DocumentAuthor;
+import pl.edu.icm.cermine.tools.timeout.TimeoutException;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App{
+	 
+	List<String> title = new ArrayList<String>();
+	List<String> journal = new ArrayList<String>();
+	List<String> year = new ArrayList<String>();
+	List<List<DocumentAuthor>> authors = new ArrayList<List<DocumentAuthor>>();
+	List<String> filep = new ArrayList<String>();
+	List<String> authorToS = new ArrayList<String>();
+	int i=0;
+	
+	
+	public void startExtract(File f) throws AnalysisException, IOException, TimeoutException, TransformationException, InterruptedException{
+		ContentExtractor extractor = new ContentExtractor();
+        InputStream inputStream=new FileInputStream(f);
+        extractor.setPDF(inputStream);
+        //TimeUnit.SECONDS.sleep(60);
+        //System.out.println(extractor.getMetadata().getTitle());
+        title.add(extractor.getMetadata().getTitle());
+        //System.out.println(extractor.getMetadata().getJournal());
+        journal.add(extractor.getMetadata().getJournal());
+       // System.out.println(extractor.getMetadata().getDate(DateType.PUBLISHED).getYear());
+        year.add(extractor.getMetadata().getDate(DateType.PUBLISHED).getYear());
+        authors.add(extractor.getMetadata().getAuthors());
+	}
+	public void getHtml(List<String> title2, List<String> journal2, List<String> year2, List<String> authorToS2, List<String> filep2){
+		List<String> list = new ArrayList<String>();
+
+		String documentPre = "<html><style> table, th, td { border: 1px solid black; </style> </head> <body>";
+		list.add(documentPre);
+
+		String headerColumn = "<table> <tr> <th>Title</th> <th>Journal</th> <th>Year</th> <th>Author</th> </tr>";
+		list.add(headerColumn);
+
+		for(int i=0; i < title2.size(); i++) {
+			
+
+		String htmlColumn = "<tr> <a href="+ filep2.get(i)+">"+ "<td>"+title2.get(i)+"</td></a> <td>"+journal2.get(i)+"<td>"+year2.get(i)+"<td>"+authorToS2.get(i)+"</td>";
+		list.add(htmlColumn);
+
+		}
+
+		String documentPost = " </table></body></html>";
+		list.add(documentPost );
+		File f = new File("Covid19ScientificArticles/data.html");
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+
+		    for (String html : list) {
+		        bw.write(html);
+		    }
+
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+	}
+	
+	
+	public static void main( String[] args ) throws AnalysisException, IOException, TimeoutException, TransformationException, InterruptedException {
+		App a = new App();
+		File f = new File("Covid19ScientificArticles/");
+		File[] listOfFiles = f.listFiles();		
+		for(File ff:listOfFiles) {
+			a.startExtract(ff);
+			a.filep.add(ff.getName());
+		}
+		for(int i=0; i<a.authors.size(); i++) {
+			System.out.println(a.title.get(i));
+			System.out.println(a.journal.get(i));
+			System.out.println(a.year.get(i));
+			String s = "";
+			for(DocumentAuthor auth:a.authors.get(i)) {
+				System.out.println(auth.getName());
+				s = (s + auth.getName() + ". ");
+				
+			}
+			a.authorToS.add(s);
+			System.out.println(a.authorToS.get(i));
+			System.out.println(a.filep.get(i));
+		}
+		a.getHtml(a.title, a.journal, a.year, a.authorToS, a.filep);
+        System.out.println( "Hello World!" );	
+    
+	}
+}
