32b4ec5326904d91f748de051ff1f1ce146173ae
Nuno Rego <nuno.rego@outlook.pt>
2020-06-02 18:16:46 +0100

    Initial API arquitecture setup

---
+++ java/covid-sci-discoveries/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ./covid-sci-discoveries
+mvn exec:java -Dexec.mainClass=covid_sci_discoveries.covid_sci_discoveries.Main
---
+++ java/run.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+from flask_restful import Resource, Api
+import os
+
+app = Flask(__name__)
+api = Api(app)
+
+@app.route("/covid-sci-discoveries")
+def get():
+    os.system('./covid-sci-discoveries/run.sh')
+    return render_template('data.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True)
