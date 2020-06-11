55b3d4620732ff0d2e1ca546eb1f33eaf2f0dd6e
Nuno Rego <nuno.rego@outlook.pt>
2020-06-11 19:23:37 +0100

    Repo

--- java/covid-sci-discoveries/run.sh
+++ java/covid-sci-discoveries/run.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+
+rm -rf /usr/src/app/java/covid-sci-discoveries/HTML/Covid_Scientific_Discoveries_Repository/*
+cp -a /usr/src/app/web/wp-content/uploads/*.pdf /usr/src/app/java/covid-sci-discoveries/HTML/Covid_Scientific_Discoveries_Repository
+ls /usr/src/app/java/covid-sci-discoveries/HTML/Covid_Scientific_Discoveries_Repository/
+
 cp /usr/src/app/templates/header.html /usr/src/app/java/covid-sci-discoveries/HTML/
 cp /usr/src/app/templates/footer.html /usr/src/app/java/covid-sci-discoveries/HTML/
+
 cd /usr/src/app/java/covid-sci-discoveries/
-pwd
 mvn exec:java -Dexec.mainClass=covid_sci_discoveries.covid_sci_discoveries.Main
-cd ..
-pwd
-rm /usr/src/app/web/Covid_Scientific_Discoveries_Repository/*
-cp -a /usr/src/app/java/covid-sci-discoveries/HTML/Covid_Scientific_Discoveries_Repository/* /usr/src/app/web/Covid_Scientific_Discoveries_Repository/
+
+rm -rf /usr/src/app/web/Covid_Scientific_Discoveries_Repository/*
+cp -a /usr/src/app/java/covid-sci-discoveries/HTML/Covid_Scientific_Discoveries_Repository/. /usr/src/app/web/Covid_Scientific_Discoveries_Repository/
 cp -a /usr/src/app/java/covid-sci-discoveries/HTML/covid-sci-discoveries.html /usr/src/app/templates/
