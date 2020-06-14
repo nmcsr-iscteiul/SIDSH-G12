3231b52cdf3859732d909067e62e2512b78918fd
tmmgo-iscteiul <56158984+tmmgo-iscteiul@users.noreply.github.com>
2020-06-14 23:57:14 +0100

    Maven tests working

---
+++ java/covid-query/src/test/java/covid_query/covid_query/AllTest.java
@@ -0,0 +1,12 @@
+package covid_query.covid_query;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(JUnitPlatform.class)
+@SelectClasses({TestApp.class, QueryFixerTest.class, XmlProjectTest.class})
+public class AllTest {
+
+}
